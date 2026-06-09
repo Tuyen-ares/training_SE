22d3242600454eb826addce69f5150486254359d
Tuyen-ares <hoantuyen2004@gmail.com>
2026-06-09 16:57:39 +0700

    props-emit

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['counting']);
+const emit = defineEmits(['handle-increase']);
+
+const onIncrease=()=>{
+    emit('handle-increase',10);
+}
+</script>
+
+<template>
+<div> Count : {{ props.counting }} </div>
+<button @click="onIncrease()">Increase</button>
+</template>
+
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue'
+
+const count = ref(10);
+
+const handleIncrease=(data)=>{
+  count.value= count.value + data;
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome :counting = "count" @handle-increase="handleIncrease"/>
+  </main>
+</template>
