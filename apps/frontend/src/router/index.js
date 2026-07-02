29b8cff85da4878c8d3ca14d94a8c41efc517a84
Tuyen-ares <hoantuyen2004@gmail.com>
2026-07-02 18:26:28 +0700

    cấu  trúc lại thư mục

---
+++ apps/frontend/src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+import { useAppStore } from '../stores/app'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+       {
+      path :'/',
+      name : 'login',
+      component : () => import('../views/login/Login.vue')
+    },
+      {
+      path :'/main',
+      name : 'main',
+      component : () => import('../views/train/Main.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/admin/Dashboard.vue'),
+      meta : { requiresAuth: true }
+    },
+     {
+      path: '/register',
+      name: 'egister',
+      component: () => import('../views/login/register.vue')},
+      {
+      path: '/CartItem',
+      name: 'CartItem',
+      component: () => import('../views/train/components/SearchBar.vue')
+    },
+  ],
+})
+
+
+router.beforeEach((to, _, next) => {
+  const appStore = useAppStore();
+  const authStore = useAuthStore()
+  appStore.setLoading(true);
+  if (to.name !== 'login' && !authStore.isAuthenticated) next({ name: 'login' })
+  else {
+    next()
+  }
+})
+
+// router.afterEach((to, from) => {
+//   const appStore = useAppStore();
+//   appStore.setLoading(false);
+// })
+
+export default router
